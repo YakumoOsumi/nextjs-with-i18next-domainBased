99c7f8a15a4adef0f7c661660cdb0d9b2e8d98cd
yakumo osumi <yakumo.osumi@g.dreamcareer.co.jp>
2025-08-15 17:56:40 +0900

    feat: 静的レンダリングできるようにした

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,7 +1,12 @@
+import {setRequestLocale} from 'next-intl/server';
 import {NextIntlClientProvider, hasLocale} from 'next-intl';
 import {notFound} from 'next/navigation';
 import {routing} from '@/i18n/routing';
  
+export function generateStaticParams() {
+  return routing.locales.map((locale) => ({locale}));
+}
+
 export default async function LocaleLayout({
   children,
   params
@@ -14,6 +19,9 @@ export default async function LocaleLayout({
   if (!hasLocale(routing.locales, locale)) {
     notFound();
   }
+
+  // Enable static rendering
+  setRequestLocale(locale);
  
   return (
     <html lang={locale}>
